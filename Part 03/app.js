f69686a70810a98c8e37c20a19da763dea193cf8
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2023-12-11 20:07:56 +0600

    Add files via upload

---
+++ Part 03/app.js
@@ -0,0 +1,15 @@
+let firstName = 'Ali';
+let age = 30;
+
+firstName = 'Shovo';
+FirstName = 'Hossain';
+firstname = 'ali';
+
+console.log(firstName);
+
+console.log(age + 50);
+
+function funcName() {
+  console.log('My First Function');
+}
+funcName();
