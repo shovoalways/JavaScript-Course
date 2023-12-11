6f09f019d5c6dfa353689d664d9bcc084b26318b
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2023-12-11 20:18:00 +0600

    Add files via upload

---
+++ 004_JS_Comments/app.js
@@ -0,0 +1,25 @@
+// My First Comment
+
+/*
+ * Multi Line Comment
+? Some info
+! Waring
+TODO: Some thing I need to fix
+ */
+
+let firstName = 'Ali';
+let age = 30;
+
+// firstName = 'Shovo'; // Changing firstName
+FirstName = 'Hossain';
+firstname = 'ali';
+/*
+console.log(firstName);
+
+console.log(age + 50);
+*/
+
+function funcName() {
+  console.log('My First Function');
+}
+funcName();
