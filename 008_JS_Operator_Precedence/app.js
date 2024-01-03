58e8305edf6f4b837231da6a3b4462832a9662b8
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2024-01-03 20:35:26 +0600

    Add files via upload

---
+++ 008_JS_Operator_Precedence/app.js
@@ -0,0 +1,13 @@
+// JavaScript Operator precedence
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
+// ----- () {} []
+// / * - +
+
+
+/*
+5 + 6 - 3 / 2 * 4
+= 5+6-1.5*4
+= 5+6-6
+= 5 
+console.log(5 + 6 - 3 / 2 * 4); // Ans: 5
+*/
