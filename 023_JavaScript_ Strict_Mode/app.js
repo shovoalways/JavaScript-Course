cc8d0b9d26bfd4d293483e440bee0e0aa6e8fc3d
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2024-05-03 21:57:00 +0600

    Add files via upload

---
+++ 023_JavaScript_ Strict_Mode/app.js
@@ -0,0 +1,18 @@
+/*
+ * JavaScript Strict Mode
+ */
+// 'use strict';
+
+x = 5;
+console.log(x);
+
+name(5, 4);
+function name(p1, p2) {
+  // 'use strict';
+  y = 5;
+  console.log(p1, p2);
+}
+
+// let package = 'Some text';
+// let public = 'Some text';
+// let private = 'Some text';
